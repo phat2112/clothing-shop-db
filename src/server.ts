169342111d66851fdbc881e6ab2bff4498b450b3
ngphat <nguyengiaphat5781@gmail.com>
2022-10-01 13:08:39 +0700

    chore(config-server): config server

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+import express, { Application, Response } from "express";
+import mongoose from "mongoose";
+
+config();
+
+// connect database
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://phatnguyen:phatnguyen@cluster0.wkyst.mongodb.net/shop-clothing?retryWrites=true&w=majority"
+  );
+}
+
+main()
+  .then(() => console.log("database connected"))
+  .catch(console.log);
+
+const app: Application = express();
+
+app.get("/", (_, res: Response) => {
+  res.send("Hello world");
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server is running from port ${port}`));

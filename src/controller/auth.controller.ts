afb8e0e554e3ba3713a0c67e6abfa3d1640b14ee
ngphat <nguyengiaphat5781@gmail.com>
2023-01-05 18:10:27 +0700

    fix check condition get product list, fix login response

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -13,97 +13,107 @@ import User from "../ models/User";
 import Logging from "../library/logging";
 
 export const loginController = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).send({ message: "Email and Password are required" });
-  }
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Email and Password are required" });
+    }
 
-  const user = await User.findOne({ email });
-  if (user) {
-    bcrypt.compare(password, user.password, (err, response) => {
-      if (err) {
-        Logging.error(err.message);
-        return res.status(500).send({ message: "Internal server error" });
-      } else if (response) {
-        return res.status(200).send({
-          token: jwt.sign({ email }, SECRET_KEY, {
-            expiresIn: EXPIRED_IN,
-          }),
-        });
-      }
-    });
-  } else {
-    return res.status(400).send({ message: "User is not found" });
+    const user = await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, response) => {
+        if (err) {
+          Logging.error(err.message);
+          return res.status(500).send({ message: "Internal server error" });
+        } else if (response) {
+          return res.status(200).send({
+            token: jwt.sign({ email }, SECRET_KEY, {
+              expiresIn: EXPIRED_IN,
+            }),
+          });
+        }
+      });
+    } else {
+      return res.status(400).send({ message: "User is not found" });
+    }
+  } catch (err) {
+    Logging.error(err);
+    return res.status(500).send({ message: "Internal server error" });
   }
 };
 
 export const registerController = async (req: Request, res: Response) => {
-  const {
-    firstName,
-    lastName,
-    sex,
-    birthday,
-    email,
-    password,
-    confirmPassword,
-  } = req.body;
-
-  const validUserCond = [
-    {
-      cond: ![email, password, confirmPassword].every(Boolean),
-      message: "Email, Password are required",
-    },
-    {
-      cond: password !== confirmPassword,
-      message: "Password was not match",
-    },
-    {
-      cond: password.length < PASSWORD_LENGTH,
-      message: "Password is not strong enough",
-    },
-  ];
-
-  const errorFound = validUserCond.find((cond) => cond.cond);
-  if (errorFound) {
-    return res.status(400).send(errorFound.message);
-  }
-
-  const user = await User.findOne({ email });
-  if (user) {
-    return res.status(409).send({ message: "Email is already used" });
-  }
-
-  bcrypt.hash(password, SALT_ROUNDS, async (err, hash) => {
-    if (err) {
-      Logging.error(err.message);
-      return res.status(500).send({ message: "Internal serer error" });
-    }
-
-    const newUser = new User({
-      birthday,
+  try {
+    const {
       firstName,
       lastName,
-      email,
       sex,
-      password: hash,
-    });
+      birthday,
+      email,
+      password,
+      confirmPassword,
+    } = req.body;
 
-    newUser.save((err, user) => {
+    const validUserCond = [
+      {
+        cond: ![email, password, confirmPassword].every(Boolean),
+        message: "Email, Password are required",
+      },
+      {
+        cond: password !== confirmPassword,
+        message: "Password was not match",
+      },
+      {
+        cond: password.length < PASSWORD_LENGTH,
+        message: "Password is not strong enough",
+      },
+    ];
+
+    const errorFound = validUserCond.find((cond) => cond.cond);
+    if (errorFound) {
+      return res.status(400).send(errorFound.message);
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(409).send({ message: "Email is already used" });
+    }
+
+    bcrypt.hash(password, SALT_ROUNDS, async (err, hash) => {
       if (err) {
-        Logging.error(err.message);
-        return res.status(500).send({ message: "Internal serer error" });
+        throw new Error("Internal serer error");
       }
 
-      return res.status(200).send({
-        user: {
-          birthday: user.birthday,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          sex: user.sex,
-        },
+      const newUser = new User({
+        birthday,
+        firstName,
+        lastName,
+        email,
+        sex,
+        password: hash,
+      });
+
+      newUser.save((err, user) => {
+        if (err) {
+          throw new Error(err.message);
+        }
+
+        return res.status(200).send({
+          user: {
+            birthday: user.birthday,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            sex: user.sex,
+          },
+        });
       });
     });
-  });
+  } catch (err) {
+    Logging.error(err);
+    return res.status(500).send({ message: "Internal serer error" });
+  }
 };
--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,14 +1,54 @@
 import { Router } from "express";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../constants/app";
 // import {validateToken} from '../middleware/authJwt';
 import {
   loginController,
   registerController,
 } from "../controller/auth.controller";
+import User from "../ models/User";
+import Logging from "../library/logging";
 
 const router = Router();
 
 router.post("/login", loginController);
 
 router.post("/register", registerController);
+
+router.post("/current-user", async (req: Request, res: Response) => {
+  try {
+    const { token } = req.body;
+
+    if (!token) {
+      throw new Error("Token is not supplied");
+    }
+
+    const decoded = jwt.verify(token, SECRET_KEY);
+
+    if (typeof decoded !== "string") {
+      const user = await User.findOne({ email: decoded.email });
+
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      return res.status(200).send({
+        data: {
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          birthday: user.birthday,
+          sex: user.sex,
+        },
+      });
+    }
+
+    throw new Error("token decoded failed");
+  } catch (error) {
+    Logging.error(error);
+
+    return res.status(500).send({ data: "Internal server error" });
+  }
+});
 
 export default router;
